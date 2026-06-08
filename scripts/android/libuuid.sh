e70b297f9b1cf3a3a7244b950ce5da5d8a258081
Shivani1995art <shivani1995.art@gmail.com>
2026-06-08 18:35:49 +0530

    Update libuuid.sh

--- scripts/android/libuuid.sh
+++ scripts/android/libuuid.sh
@@ -17,7 +17,15 @@ fi
   --disable-fast-install \
   --host="${HOST}" || return 1
 
+
+# Patch gen_uuid.c to remove flock which is not available on Android NDK
+find "${BASEDIR}/src/${LIB_NAME}" -name "gen_uuid.c" -exec sed -i 's/while (flock(state_fd, LOCK_EX) < 0)/while (0)/g' {} \;
+find "${BASEDIR}/src/${LIB_NAME}" -name "gen_uuid.c" -exec sed -i 's/flock(state_fd, LOCK_UN);//g' {} \;
+echo "INFO: gen_uuid.c flock patch applied" 1>>"${BASEDIR}"/build.log 2>&1
+
 make -j$(get_cpu_count) || return 1
+
+
 
 make install || return 1
 
